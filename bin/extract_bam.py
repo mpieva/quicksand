2ea0daab78da0c7440a737b6168ebfcaa09e929e
Johann Visagie <johann@visagie.za.net>
2019-06-12 20:49:47 +0200

    Add extract_bam.py from Snakemake pipeline as-is

---
+++ bin/extract_bam.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser, RawTextHelpFormatter, FileType
+from pysam import AlignmentFile
+
+
+def cli():
+    parser = ArgumentParser(
+        formatter_class=RawTextHelpFormatter
+    )
+    parser.add_argument(
+        '-f', '--family', type=str,
+        help="Taxonimical family for which to extract data"
+    )
+    parser.add_argument(
+        '-k', '--krakenfile', type=FileType('r'),
+        help="Output from kraken-translate"
+    )
+    parser.add_argument(
+        '-o', '--outfile', type=str,
+        help="Output file"
+    )
+    parser.add_argument(
+        'bamfile', type=FileType('rb'),
+        help="BAM file from which to extract"
+    )
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    cfg = cli()
+
+    with cfg.krakenfile as kf:
+        seq_ids = [l.split('\t', 1)[0] for l in kf
+                   if "c__Mammalia" in l and f"f__{cfg.family}" in l]
+
+    with AlignmentFile(cfg.bamfile, 'rb') as bf:
+        with AlignmentFile(cfg.outfile, 'wb', template=bf) as of:
+            for read in bf.fetch(until_eof=True):
+                if read.query_name in seq_ids:
+                    of.write(read)
