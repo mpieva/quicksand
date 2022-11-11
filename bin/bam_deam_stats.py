7d1380de63f1edcf222c98c1eec407456faf8de5
Merlin Szymanski <merlin_szymanski@eva.mpg.de>
2022-11-11 10:59:43 +0100

    Minor fix: Display of deamination rate

--- bin/bam_deam_stats.py
+++ bin/bam_deam_stats.py
@@ -8,10 +8,10 @@ def binomial_ci(x, n, alpha=0.05):
     #Clopper-Pearson interval for binomial distribution
     #x is number of successes, n is number of trials, alpha the percent
     if n==0:
-        return None,None
+        return "N/A,N/A"
     lower = stats.beta.interval(1-alpha, x,n-x+1)[0] if x != 0 else 0
-    upper = stats.beta.interval(1-alpha, x+1,n-x)[1] if x != n else 1  
-    return round(lower*100,2), round(upper*100,2)
+    upper = stats.beta.interval(1-alpha, x+1,n-x)[1] if x != n else 1
+    return f"{round(lower*100,1):.1f},{round(upper*100,1):.1f}"
 
 
 def main(bamfile, stats_only=False):
@@ -22,7 +22,7 @@ def main(bamfile, stats_only=False):
     #store all bases from the other end of the observed deamination
     cond5_last = []
     cond3_first = []
-    
+
     n_deam_1 = 0
     n_deam_3 = 0
     #count the individual events
@@ -37,11 +37,11 @@ def main(bamfile, stats_only=False):
 
     #open the files
     infile = pysam.AlignmentFile(bamfile, 'rb')
-    
+
     if not only_stats:
         out1term = pysam.AlignmentFile('output.deaminated1.bam', 'wb', template=infile)
         out3term = pysam.AlignmentFile('output.deaminated3.bam', 'wb', template=infile)
-    
+
     #main loop
     for read in infile:
         # get read and ref sequences, reverse complement if necessary
@@ -53,25 +53,25 @@ def main(bamfile, stats_only=False):
             seq = read.get_forward_sequence()
 
         rlen = len(ref)-1 #0based required
- 
+
         all_first.append(ref[0].upper())
         all_last.append(ref[-1].upper())
 
         # check if ct substitutions in the first or last 3 postions and save indices
         mism = [
-            n for n,x in enumerate(ref)                             
-            if ((x, seq[n]) == ('c','T')) and (n<=2 or n>=rlen-2)   
+            n for n,x in enumerate(ref)
+            if ((x, seq[n]) == ('c','T')) and (n<=2 or n>=rlen-2)
         ]
 
         # skip non-damaged reads
         if len(mism) == 0:
             continue
-        
+
         # increase the deamination stats counts
-         
+
         deam51 = 0 in mism
         deam31 = rlen in mism
-        deam53 = any(x<=2 for x in mism) 
+        deam53 = any(x<=2 for x in mism)
         deam33 = any(x>=rlen-2 for x in mism)
         cond = deam51 and deam31
 
@@ -79,7 +79,7 @@ def main(bamfile, stats_only=False):
         n_deam31 += int(deam31)
         n_deam53 += int(deam53)
         n_deam33 += int(deam33)
-        n_cond   += int(cond) 
+        n_cond   += int(cond)
 
         if deam51:
             cond5_last.append(ref[-1].upper())
@@ -109,23 +109,23 @@ def main(bamfile, stats_only=False):
     all_3c_cond5 = cond3_first.count('C')
 
     #calculate deamination percentage
-    p_deam51 = round(n_deam51/all_5c * 100,2) if all_5c>0 else None
-    p_deam31 = round(n_deam31/all_3c * 100,2) if all_3c>0 else None
-    
+    p_deam51 = round(n_deam51/all_5c * 100,1) if all_5c>0 else None
+    p_deam31 = round(n_deam31/all_3c * 100,1) if all_3c>0 else None
+
     #and the confidence interval for that
     p_deam51_95ci = binomial_ci(n_deam51,all_5c)
     p_deam31_95ci = binomial_ci(n_deam31,all_3c)
 
     # percentage of deaminated conditional reads
-    p_deam51_cond = round(n_cond/all_3c_cond5 *100,2) if all_3c_cond5>0 else None
-    p_deam31_cond = round(n_cond/all_5c_cond3 *100,2) if all_5c_cond3>0 else None
-    
+    p_deam51_cond = round(n_cond/all_3c_cond5 *100,1) if all_3c_cond5>0 else None
+    p_deam31_cond = round(n_cond/all_5c_cond3 *100,1) if all_5c_cond3>0 else None
+
     # the 95% confidence interval
     p_deam51_cond_95ci = binomial_ci(n_cond,all_3c_cond5)
     p_deam31_cond_95ci = binomial_ci(n_cond,all_5c_cond3)
-    
+
     ancientness = '-'
-    test51,test31 = p_deam51_95ci[0],p_deam31_95ci[0]
+    test51,test31 = float(p_deam51_95ci.split(',')[0]),float(p_deam31_95ci.split(',')[0])
     if test51 and test31:
         if test51 > 9.5 or test31 > 9.5:
             ancientness = '+'
@@ -135,18 +135,24 @@ def main(bamfile, stats_only=False):
     #And the report
     #print header
     print(
-        "Ancientness", "ReadsDeam(1term)","ReadsDeam(3term)", 
+        "Ancientness", "ReadsDeam(1term)","ReadsDeam(3term)",
         "Deam5(95ci)", "Deam3(95ci)", "Deam5Cond(95ci)",
         "Deam3Cond(95ci)", sep='\t',file=sys.stdout
         )
 
+    def pprint(var):
+        try:
+            return f"{var:.1f}"
+        except: 
+            return 'N/A'
+
     #and row
     print(
-        ancientness, n_deam_1, n_deam_3, 
-        f"{p_deam51} {p_deam51_95ci}".replace(", ",",").replace("None","N/A"),
-        f"{p_deam31} {p_deam31_95ci}".replace(", ",",").replace("None","N/A"), 
-        f"{p_deam51_cond} {p_deam51_cond_95ci}".replace(", ",",").replace("None","N/A"),
-        f"{p_deam31_cond} {p_deam31_cond_95ci}".replace(", ",",").replace("None","N/A"), 
+        ancientness, n_deam_1, n_deam_3,
+        f"{pprint(p_deam51)} ({p_deam51_95ci})",
+        f"{pprint(p_deam31)} ({p_deam31_95ci})",
+        f"{pprint(p_deam51_cond)} ({p_deam51_cond_95ci})",
+        f"{pprint(p_deam31_cond)} ({p_deam31_cond_95ci})",
         sep='\t', file=sys.stdout
         )
 
